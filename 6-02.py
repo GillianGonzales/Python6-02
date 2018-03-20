eae24cd2eeb93636a1e995182be56202e7d41377
Gillian <gillian.gonzales@ocsbstudent.ca>
2018-03-20 14:31:20 -0400

    Initial commit

---
+++ 6-02.py
@@ -0,0 +1,27 @@
+#
+#
+# Created By Gillian Gonzalesa
+# Created On March 20 2018
+#
+# This program will tell if a number is postive, negative, zero
+#
+
+#Instructions
+number = int(input("Type a number in "))
+
+#Assigning Varibles
+zero = 0
+
+#Making Function
+if number > zero :
+	print("Postive")
+
+elif number < zero :
+	print("Negative")
+
+elif number == zero :
+	print("Zero")
+pass
+
+print("")
+input("End program")
